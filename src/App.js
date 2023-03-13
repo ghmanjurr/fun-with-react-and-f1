eaed5c8c146bd2b24b00e509db7a972cbccfa36c
ghmanjurr <manjurr.ghalib@gmail.com>
2023-03-13 15:32:43 -0700

    page link to f1tv, updated logo art

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/*Edit <code>src/App.js</code> and save to reload.*/}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://f1tv.formula1.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Watch F1 TV
         </a>
       </header>
     </div>
