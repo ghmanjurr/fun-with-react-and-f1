6b4515fb6076ea2e8a4dc63206fa14cbff741b72
ghmanjurr <manjurr.ghalib@gmail.com>
2023-03-21 10:15:10 -0700

    added banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/banner/banner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Banner/>
+        
         <p>
           {/*Edit <code>src/App.js</code> and save to reload.*/}
         </p>
---
+++ src/components/banner/banner.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Banner = () => {
+    const [expand, setExpand] = useState(false);
+    const toggleExpand = () => setExpand(prevExpand => !prevExpand)
+    
+    return <div className="banner">
+        <button onClick={toggleExpand}>F1TV banner  <span>+</span></button>
+        { expand && <div className="bannerLink">Link to F1tv website</div>}
+    </div>
+}
+
+export default Banner;
