152903f4b9b1dbd61130a7f3d61ba016d2c4d0e4
ghmanjurr <manjurr.ghalib@gmail.com>
2023-03-22 11:06:29 -0700

    added const list of current F1 drivers' roster

---
+++ src/components/drivers.js
@@ -0,0 +1,142 @@
+export const drivers = [
+    {
+        id: 1,
+        name: 'Max Verstappen',
+        tag: 'VER',
+        driverNum: '1',
+        team: 'Red Bull'        
+    },
+    {
+        id: 2,
+        name: 'Sergio Perez'
+        tag: 'PER'
+        driverNum: '11'
+        team: 'Red Bull'        
+    },
+    {
+        id: 3,
+        name: 'Charles Leclerc',
+        tag: 'LEC',
+        driverNum: '16',
+        team: 'Ferrari'        
+    },
+    {
+        id: 4,
+        name: 'Carlos Sainz'
+        tag: 'SAI'
+        driverNum: '55'
+        team: 'Ferrari'        
+    },   
+    {
+        id: 5,
+        name: 'Fernando Alonso',
+        tag: 'ALO',
+        driverNum: '14',
+        team: 'Aston Martin'        
+    },
+    {
+        id: 6,
+        name: 'Lance Stroll'
+        tag: 'STR'
+        driverNum: '18'
+        team: 'Aston Martin'        
+    },
+    {
+        id: 7,
+        name: 'Lewis Hamilton',
+        tag: 'HAM',
+        driverNum: '44',
+        team: 'Mercedes'        
+    },
+    {
+        id: 8,
+        name: 'George Russell'
+        tag: 'RUS'
+        driverNum: '63'
+        team: 'Mercedes'        
+    },
+    {
+        id: 9,
+        name: 'Nyck de Vries',
+        tag: 'DEV',
+        driverNum: '21',
+        team: 'AlphaTauri'        
+    },
+    {
+        id: 10,
+        name: 'Yuki Tsunoda',
+        tag: 'TSU',
+        driverNum: '22',
+        team: 'AlphaTauri'        
+    },
+    {
+        id: 11,
+        name: 'Valtteri Bottas',
+        tag: 'BOT',
+        driverNum: '77',
+        team: 'Alfa Romeo'        
+    },
+    {
+        id: 12,
+        name: 'Zhou Guanyu',
+        tag: 'ZHO',
+        driverNum: '24',
+        team: 'Alfa Romeo'        
+    },
+    {
+        id: 13,
+        name: 'Esteban Ocon',
+        tag: 'OCO',
+        driverNum: '31',
+        team: 'Alpine'        
+    },
+    {
+        id: 14,
+        name: 'Pierre Gasly',
+        tag: 'GAS',
+        driverNum: '10',
+        team: 'Alpine'        
+    },
+    {
+        id: 15,
+        name: 'Kevin Magnussen',
+        tag: 'MAG',
+        driverNum: '20',
+        team: 'Haas'        
+    },
+    {
+        id: 16,
+        name: 'Nico Hulkenberg',
+        tag: 'HUL',
+        driverNum: '27',
+        team: 'Haas'        
+    },
+    {
+        id: 17,
+        name: 'Alexander Albon',
+        tag: 'ALB',
+        driverNum: '23',
+        team: 'Williams'        
+    },
+    {
+        id: 18,
+        name: 'Logan Sargeant',
+        tag: 'SAR',
+        driverNum: '2',
+        team: 'Williams'        
+    },
+    {
+        id: 19,
+        name: 'Lando Norris',
+        tag: 'NOR',
+        driverNum: '4',
+        team: 'McLaren'        
+    },
+    {
+        id: 20,
+        name: 'Oscar Piastri',
+        tag: 'PIA',
+        driverNum: '81',
+        team: 'McLaren'        
+    },
+];
