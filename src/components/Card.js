0735d02453b07df1e18dbb96f0a8b8da63c90e5f
ghmanjurr <manjurr.ghalib@gmail.com>
2023-03-22 11:17:34 -0700

    created Card component

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = ({name, team, driverNum, tag, id}) => {
+    return(
+        <div>
+            <img alt='drivers' src={`https://robohash.org/${id}?200x200`}/>
+            <div>
+                <h2>{name}</h2>
+                <h3>{team}</h3>
+                <p>{driverNum}</p>
+                <p>{tag}</p>
+            </div>
+        </div>
+    )
+}
